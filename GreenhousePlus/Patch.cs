3b7279b9b7715c473cb5af84b19643a9bd688335
Fireball14 <Fireball14@universinet.org>
2022-02-07 03:10:45 +0200

    GreenhousePlus Version 1.0.0 added

---
+++ GreenhousePlus/Patch.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+
+namespace GreenhousePlus
+{
+    public class Patch
+    {
+        static AccessTools.FieldRef<Object_Planter, float> m_minTemperature = AccessTools.FieldRefAccess<Object_Planter, float>("m_minTemperature");
+        static AccessTools.FieldRef<Object_Planter, float> m_maxTemperature = AccessTools.FieldRefAccess<Object_Planter, float>("m_maxTemperature");
+        
+        [HarmonyPatch(typeof(Object_Planter), "PlantSeed")]
+        [HarmonyPostfix]
+        public static void PlantSeed_Postfix(Object_Planter __instance, ObjectManager.ObjectType  ___m_objectType)
+        {
+            float minTemperatureMod = 0;
+            float maxTemperatureMod = 0;
+            switch (___m_objectType)
+            {
+                case ObjectManager.ObjectType.Greenhouse:
+                    minTemperatureMod = -5;
+                    maxTemperatureMod = 5;
+                    break;
+                case ObjectManager.ObjectType.LargeGreenhouse:
+                    minTemperatureMod = -10;
+                    maxTemperatureMod = 10;
+                    break;
+                case ObjectManager.ObjectType.HugeGreenhouse:
+                    minTemperatureMod = -15;
+                    maxTemperatureMod = 15;
+                    break;
+            }
+            m_minTemperature(__instance) += minTemperatureMod;
+            m_maxTemperature(__instance) += maxTemperatureMod;
+        }
+    }
+}
---
+++ GreenhousePlus/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace GreenhousePlus
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Sheltered2.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Harmony harmony = new (PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(Patch));
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
